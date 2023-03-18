ed67a557758b29ae3758dd45a2242a68306aea60
Jonyelson Lopes <jonyelson20@gmail.com>
2023-03-18 14:42:09 -0300

    fix: jogo teco

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -18,7 +18,7 @@ export function Header({ aposta }: TotalApostaProps) {
   return (
     <HeaderContainer>
       <img src={logo} alt="logo x1 da baixada" />
-      <h1>Oséias☘️ x Focka🦭</h1>
+      <h1>T MUller x Preto</h1>
       <div>
         <h3>
           <FontAwesomeIcon icon={faHandHoldingDollar} size="lg" />
